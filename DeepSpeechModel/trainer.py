5801654c4f9f2ff86fb50910f3757e709fef9364
bello <bmtukur621@gmail.com>
2022-09-14 13:22:01 +0100

    added training functions, data loaders and more

--- DeepSpeechModel/trainer.py
+++ DeepSpeechModel/trainer.py
@@ -1,7 +1,10 @@
 from torch.utils.data import DataLoader
 import torch.optim as optim
 import torch.nn.functional as F
+import torch
+from torch.utils import data
 
+from utils import data_processor
 
 
 class Trainer:
@@ -19,3 +22,45 @@ class Trainer:
 
     def train(self):
         pass
+
+
+
+
+
+
+learning_rate = 5e-4
+batch_size = 20
+epochs = 50
+hparams = {
+        "n_cnn_layers": 3,
+        "n_rnn_layers": 5,
+        "rnn_dim": 512,
+        "n_class": 29,
+        "n_feats": 128,
+        "stride":2,
+        "dropout": 0.1,
+        "learning_rate": learning_rate,
+        "batch_size": batch_size,
+        "epochs": epochs
+    }
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+kwargs = {'num_workers': 1, 'pin_memory': True} if torch.cuda.is_available() else {}
+
+
+train_dataset = None
+test_dataset = None
+
+train_loader = data.DataLoader(dataset=train_dataset,
+                                batch_size=hparams['batch_size'],
+                                shuffle=True,
+                                collate_fn=lambda x: data_processor(x, True),
+                                **kwargs)
+
+
+test_loader = data.DataLoader(dataset = test_dataset,
+                              batch_size = hparams['batch_size'],
+                              shuffle=False,
+                              collate_fn=lambda x: data_processor(x, False),
+                              **kwargs)
+
+
--- DeepSpeechModel/utils.py
+++ DeepSpeechModel/utils.py
@@ -157,3 +157,67 @@ def data_processor(data,text_transforms,train=True):
     return spectrograms,labels,input_len,label_len
 
 
+
+
+def train(model,device,train_loader,criterion,optimizer,scheduler,epoch,iter_meter):
+    
+    model.train()
+    data_len = len(train_loader.dataset)
+
+    for batch_idx, data in enumerate(train_loader):
+        spectrograms, labels, input_lengths, label_lengths = data 
+        spectrograms, labels = spectrograms.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+
+        out = model(spectrograms) # (batch, time, n_class)
+        out= F.log_softmax(out,dim=2)
+        output = out.transpose(0, 1) # (time, batch, n_class)
+
+        loss = criterion(output, labels, input_lengths, label_lengths)
+        loss.backward()
+
+        optimizer.step()
+        scheduler.step()
+        iter_meter.step()
+
+        if batch_idx % 100 == 0 or batch_idx == data_len:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(spectrograms), data_len,
+                    100. * batch_idx / len(train_loader), loss.item()))
+            
+
+
+            
+def test(model,device,test_loader,criterion,epoch, iter_meter):
+    print('\nevaluating...')
+    model.eval()
+    test_loss = 0
+    test_cer, test_wer = [], []
+    cer = CharErrorRate()
+    wer = WordErrorRate()
+    
+    with torch.no_grad():
+        for idx, data in enumerate(test_loader):
+            spectrograms, labels, input_lengths, label_lengths = data 
+            spectrograms, labels = spectrograms.to(device), labels.to(device)
+         
+            out = model(spectrograms) # (batch, time,n_class)
+            out = F.log_softmax(out,dim=2)
+            output = out.transpose(0,1) # (time,batch, n_class)
+
+            loss = criterion(output, labels, input_lengths, label_lengths)
+
+            test_loss += loss.item() / len(test_loader)
+
+            decoded_preds, decoded_targets = GreedyDecoder(output.transpose(0, 1), labels, label_lengths)
+
+            for j in range(len(decoded_preds)):
+                test_cer.append(cer(decoded_preds[j],decoded_targets[j]))
+                test_wer.append(wer(decoded_preds[j],decoded_targets[j]))
+
+    
+    avg_cer = sum(test_cer)/len(test_cer)
+    avg_wer = sum(test_wer)/len(test_wer)
+    
+    print('Test set: Average loss: {:.4f}, Average CER: {:4f} Average WER: {:.4f}\n'.format(test_loss, avg_cer, avg_wer))
